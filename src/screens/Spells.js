ae243943974a916b394aff499eedc62f83816cbd
Wes Galbraith <wes@Wess-MBP.fios-router.home>
2024-01-01 22:40:31 -0700

    Add some styling to the spell screen

--- src/screens/Spells.js
+++ src/screens/Spells.js
@@ -1,40 +1,85 @@
 import {useState} from 'react'
-import { Text, StyleSheet, FlatList, TextInput, Pressable, View } from 'react-native'
+import { Text, StyleSheet, FlatList, TextInput, Pressable, View, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useSpell } from '../hooks/useSpell'
+import { theme } from '../theme'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
 
 const Spells = ({spells, numberOptions}) => {
     const {spell, error, fetchSpellByIndex} = useSpell()
     const [query, setQuery] = useState('')
-    options = selectOptions(query, spells, numberOptions)
+    const [searchResultsOpen, setOptionTrayOpen] = useState(false)
+    const [searchResultsTop, setSearchResultsTop] = useState([0, 0])
 
+    const options = selectOptions(query, spells, numberOptions)
+    const [searchResultsX, searchResultsY] = searchResultsTop
 
     return (
         <SafeAreaView style={styles.container}>
+            <MaterialCommunityIcons name="wizard-hat" size={theme.font.size.extraLarge} color={theme.colors.text} />
             <TextInput 
-                style={styles.textInput}
+                onLayout={({nativeEvent}) => {
+                    if (nativeEvent.layout) {
+                        const {x, y, height} = nativeEvent.layout
+                        setSearchResultsTop([x, y + height])
+                    }
+                }}
+                style={styles.searchBar}
                 placeholder='search for spells'
                 placeholderTextColor='grey'
-                onChangeText={setQuery}
-            />
-            <FlatList
-                data={options} 
-                renderItem={({item}) => {
-                    return (
-                        <Item 
-                            name={item.name} 
-                            index={item.index}
-                            onPress={() => fetchSpellByIndex(item.index)}
-                        />
-                    )
+                onChangeText={q => {
+                    setQuery(q)
+                    if (!searchResultsOpen) {
+                        setOptionTrayOpen(true)
+                    }
+                    if (searchResultsOpen && !q) {
+                        setOptionTrayOpen(false)
+                    }
                 }}
-                keyExtractor={item => item.index}
             />
             {
+                searchResultsOpen && (
+                    <FlatList
+                        data={options} 
+                        style={[styles.searchBarResults, {
+                            position: 'absolute',
+                            left: searchResultsX,
+                            top: searchResultsY,
+                            zIndex: 1,
+                        }]}
+                        renderItem={({item}) => {
+                            return (
+                                <Item 
+                                    name={item.name} 
+                                    index={item.index}
+                                    // TODO: make pressing a search result dismiss the keyboard
+                                    onPress={() => {
+                                        fetchSpellByIndex(item.index)
+                                        setQuery('')
+                                        setOptionTrayOpen(false)
+                                    }}
+                                />
+                            )
+                        }}
+                        keyExtractor={item => item.index}
+                    />
+                )
+            }
+            {
                 spell && (
-                    <View style={styles.spellContent}>
+                    <ScrollView contentContainerStyle={styles.spellContent}>
+                        {/* TODO: add more information about each spell */}
                         <Text style={styles.spellTitle}>{spell.name}</Text>
-                    </View>
+                        {
+                            spell.desc?.map(text => {
+                                return (
+                                    <Text style={styles.paragraph}>{text}</Text>
+                                )
+                            })
+                        }
+                        <Text></Text>
+                    </ScrollView>
                 )
             }
 
@@ -47,7 +92,7 @@ const Item = ({name, index, onPress}) => {
         <Pressable
             onPress={onPress}
         >
-            <Text>{name}</Text>
+            <Text style={styles.searchResultsItem}>{name}</Text>
         </Pressable>
     )
 }
@@ -75,26 +120,58 @@ const selectOptions = (query, spells, limit) => {
 
 const styles = StyleSheet.create({
     container: {
-        background: 'red',
+        width: '100%',
+        backgroundColor: theme.colors.primary,
         flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        
     },
-    textInput: {
-        height: 40,
-        margin: 12,
+    searchBar: {
+        height: 45,
+        backgroundColor: theme.colors.primaryShade,
+        color: theme.colors.text,
+        fontSize: theme.font.size.small,
+        marginTop: 10,
+        marginBottom: 20,
+        padding: 10,
+        width: '60%',
         borderWidth: 1,
-        padding: 10,        
+        borderColor: theme.colors.accent,
+        borderRadius: 5,
+    },
+    searchBarResults: {
+        backgroundColor: theme.colors.primaryShade,
+        width: '60%',
+        paddingBottom: 10,
+    },
+    searchResultsItem: {
+        color: theme.colors.accent,
+        width: '100%',
+        height: 30,
+        fontSize: theme.font.size.small,
+        marginTop: 5,
+        paddingHorizontal: 10, 
     },
     spellContent: {
         flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginTop: 20,
+        padding: 10,
     },
     spellTitle: {
-        fontSize: 40,
+        color: theme.colors.text,
+        fontSize: theme.font.size.large,
         fontWeight: 'bold',
-        backgroundColor: 'purple',
         minHeight: 40,
         width: '100%',
+    },
+    paragraph: {
+        color: theme.colors.text,
+        fontSize: theme.font.size.small,
+        marginTop: 10,
     }
 })
 
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,8 @@
 export const theme = {
     colors: {
         primary: '#333745', // applied to headers, backgrounds, and large sections
+        primaryTint: '#4A5064', // lighter variant of primary color
+        primaryShade: '#22252E', // darker variant of primary color
         secondary: '#FE5F55', // applied to buttons, borders, and smaller components
         accent: '#C7EFCF', // use sparingly to draw attention to important elements
         error: '#E63462', 
@@ -11,6 +13,13 @@ export const theme = {
             light: 200,
             normal: 400,
             bold: 700,
-        }
+        },
+        size: {
+            extraSmall: 12,
+            small: 18,
+            medium: 24,
+            large: 30,
+            extraLarge: 36,
+        },
     },
 }
